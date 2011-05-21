05ec07f8e5d596266f0d929ba07ccda219270ab4
Spencer Steffen <spencer@citrusme.com>
2011-05-21 02:45:28 -0700

    improved search run!

--- lib/craiger/search.rb
+++ lib/craiger/search.rb
@@ -2,27 +2,24 @@ module Craiger
 
   class Search
     
-    attr_reader :query
+    attr_reader :query, :results
     
     def initialize(query, options={})
       @query = options
       @query[:query] = query.to_s.strip.gsub(/\s+/, " + ")
     end
-        
-    def results
-      @results ||= []
-    end
+    
     
     def run!
       puts "Searching for #{@query}!"
-      
+      @results = []
       @cities = Locations.all #.shuffle.take(10)
       
       count = 0
-      per_group = 7
+      per_group = 25
       total  = @cities.length
-        
-        
+      
+              
       until @cities.empty? do
       
         EM.run {
@@ -36,39 +33,41 @@ module Craiger
           end
           
           multi.callback {
-            puts "+"            
+            
             multi.responses[:failed].each do |response|
               puts "\n" * 4
               puts "response to #{response} failed"
               pp response
               puts "\n" * 4
             end
+            
             @parser = Parser.new multi.responses[:succeeded].map(&:response)
+            @results += @parser.results
             
-            results << @parser.results
+            print "+ "
+            print @parser.results.length
+            print " = "
+            puts @results.length
             
-            EM.stop            
+            EM.stop
+                        
           }
+          trap("INT") { EM.stop; break }
         }
-        trap("INT") { EM.stop }
       end
-       
+      
       display
       #email
                   
     end
   
     def display
-      
       puts "\n\n"
       puts "RESULTS:"
-      results.flatten.each do |result|
+      @results.each do |result|
         puts "-" * 88
         puts result
       end
-      
-      #puts results.flatten.join("\n")
-      
       puts "-" * 88
       puts "\n\n"
     end
@@ -79,7 +78,7 @@ module Craiger
         :to        => EMAIL_TO,
         :from      => 'craiger@citrusme.com',
         :subject   => "Search results for #{query}",
-        :body      =>  results.flatten.join("\n\n"),
+        :body      =>  @results.join("\n\n"),
         :charset   => 'utf8',
         :via       => :sendmail
       )
